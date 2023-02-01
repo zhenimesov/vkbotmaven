7616b1cb6e0a5163f1326dc40c3ae849b360ab6b
Ferius057 <ferius057@gmail.com>
2023-02-01 16:05:29 +0600

    perf: обновлена производительность команды профиль

--- src/main/java/kz/tamoha/vkbotmaven/command/Profile.java
+++ src/main/java/kz/tamoha/vkbotmaven/command/Profile.java
@@ -5,6 +5,7 @@ import api.longpoll.bots.model.objects.basic.Message;
 import kz.tamoha.vkbotmaven.command.api.annotation.CommandAnnotation;
 import kz.tamoha.vkbotmaven.command.api.impl.AbstractCommand;
 import kz.tamoha.vkbotmaven.command.api.model.CacheDataMessage;
+import kz.tamoha.vkbotmaven.model.basic.FullName;
 import kz.tamoha.vkbotmaven.model.basic.User;
 import kz.tamoha.vkbotmaven.model.media.MessageTextData;
 
@@ -18,55 +19,39 @@ public class Profile extends AbstractCommand {
 
     @Override
     public void run(CacheDataMessage cache, Message message, String[] args) throws VkApiException {
-        vk.messages.send()
-                .setPeerId(message.getPeerId())
-                .setMessage(MessageTextData.PROFILE.getText()
-                        .replace("%id%", String.valueOf(cache.getSender().getId()))
-
-                        .replace("%p_0%", cache.getSender().getFullName().get(0).getPush())
-                        .replace("%p_1%", cache.getSender().getFullName().get(1).getPush())
-                        .replace("%p_2%", cache.getSender().getFullName().get(2).getPush())
-                        .replace("%p_3%", cache.getSender().getFullName().get(3).getPush())
-                        .replace("%p_4%", cache.getSender().getFullName().get(4).getPush())
-                        .replace("%p_5%", cache.getSender().getFullName().get(5).getPush())
-                        .replace("%np_0%", cache.getSender().getFullName().get(0).getNoPush())
-                        .replace("%np_1%", cache.getSender().getFullName().get(1).getNoPush())
-                        .replace("%np_2%", cache.getSender().getFullName().get(2).getNoPush())
-                        .replace("%np_3%", cache.getSender().getFullName().get(3).getNoPush())
-                        .replace("%np_4%", cache.getSender().getFullName().get(4).getNoPush())
-                        .replace("%np_5%", cache.getSender().getFullName().get(5).getNoPush())
-
-                        .replace("%gender%", cache.getSender().getGender().getValue())
-
-                        .replace("%perm%", String.valueOf(cache.getSender().getPermission())))
-                .execute();
+       getProfile(cache.getSender(), message.getPeerId());
     }
 
     @Override
     public void run(CacheDataMessage cache, Message message, List<Message> replyMessages, String[] args) throws VkApiException {
         for (User replySender : cache.getReplySenders()) {
-            vk.messages.send()
-                    .setPeerId(message.getPeerId())
-                    .setMessage(MessageTextData.PROFILE.getText()
-                            .replace("%id%", String.valueOf(cache.getSender().getId()))
-
-                            .replace("%p_0%", replySender.getFullName().get(0).getPush())
-                            .replace("%p_1%", replySender.getFullName().get(1).getPush())
-                            .replace("%p_2%", replySender.getFullName().get(2).getPush())
-                            .replace("%p_3%", replySender.getFullName().get(3).getPush())
-                            .replace("%p_4%", replySender.getFullName().get(4).getPush())
-                            .replace("%p_5%", replySender.getFullName().get(5).getPush())
-                            .replace("%np_0%", replySender.getFullName().get(0).getNoPush())
-                            .replace("%np_1%", replySender.getFullName().get(1).getNoPush())
-                            .replace("%np_2%", replySender.getFullName().get(2).getNoPush())
-                            .replace("%np_3%", replySender.getFullName().get(3).getNoPush())
-                            .replace("%np_4%", replySender.getFullName().get(4).getNoPush())
-                            .replace("%np_5%", replySender.getFullName().get(5).getNoPush())
-
-                            .replace("%gender%", replySender.getGender().getValue())
-
-                            .replace("%perm%", String.valueOf(replySender.getPermission())))
-                    .execute();
+            getProfile(replySender, message.getPeerId());
         }
+    }
+
+    private void getProfile(User user, int peerId) throws VkApiException {
+        List<FullName> fullName = user.getFullName();
+        vk.messages.send()
+                .setPeerId(peerId)
+                .setMessage(MessageTextData.PROFILE.getText()
+                        .replace("%id%", String.valueOf(user.getId()))
+
+                        .replace("%p_0%", fullName.get(0).getPush())
+                        .replace("%p_1%", fullName.get(1).getPush())
+                        .replace("%p_2%", fullName.get(2).getPush())
+                        .replace("%p_3%", fullName.get(3).getPush())
+                        .replace("%p_4%", fullName.get(4).getPush())
+                        .replace("%p_5%", fullName.get(5).getPush())
+                        .replace("%np_0%", fullName.get(0).getNoPush())
+                        .replace("%np_1%", fullName.get(1).getNoPush())
+                        .replace("%np_2%", fullName.get(2).getNoPush())
+                        .replace("%np_3%", fullName.get(3).getNoPush())
+                        .replace("%np_4%", fullName.get(4).getNoPush())
+                        .replace("%np_5%", fullName.get(5).getNoPush())
+
+                        .replace("%gender%", user.getGender().getValue())
+
+                        .replace("%perm%", String.valueOf(user.getPermission())))
+                .execute();
     }
 }
